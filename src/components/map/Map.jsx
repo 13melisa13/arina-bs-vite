bd0d271fc7e37b010ad6f814cabf791716702c2a
Isaeva Adelina <isaeva.adelya2016@ya.ru>
2025-02-21 22:05:35 +0300

    fix: fix path of images

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties} from 'react';
+
 import styles from './map.module.scss';
 
 export default function Map() {
@@ -7,7 +7,7 @@ export default function Map() {
             <iframe
                 className={styles.map}
                 src="https://yandex.ru/map-widget/v1/?um=constructor%3A48aea63395eb1b6981b34c1da0e3fc8a2084eb70837e1f7bc16cf6a72fe064bf&amp;source=constructor"
-                frameBorder="0"></iframe>
+                ></iframe>
         </aside>
     )
 }
